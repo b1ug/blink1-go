9b61e9792091d0ca2554aaedc10e8124255aff6d
hyorigo <67533928+hyorigo@users.noreply.github.com>
2023-10-08 17:53:38 +0800

    [feat] parse state query/description (in English) like human (#6)
    * For parser draft
    
    * Add comments
    
    * Rename and add blank tests
    
    * Add test cases
    
    * all pink now
    
    * Remove dup
    
    * Better typing, and fix lint
    
    * Add examples
    
    * Refine func comments

---
+++ parser.go
@@ -0,0 +1,255 @@
+package blink1
+
+import (
+	"errors"
+	"fmt"
+	"image/color"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+var colorMap = map[string]color.Color{
+	"beige":   ColorBeige,
+	"black":   ColorBlack,
+	"blue":    ColorBlue,
+	"brown":   ColorBrown,
+	"cyan":    ColorCyan,
+	"aqua":    ColorCyan,
+	"gold":    ColorGold,
+	"gray":    ColorGray,
+	"grey":    ColorGray,
+	"green":   ColorGreen,
+	"indigo":  ColorIndigo,
+	"lime":    ColorLime,
+	"magenta": ColorMagenta,
+	"fuchsia": ColorMagenta,
+	"maroon":  ColorMaroon,
+	"mint":    ColorMint,
+	"navy":    ColorNavy,
+	"olive":   ColorOlive,
+	"orange":  ColorOrange,
+	"pink":    ColorPink,
+	"purple":  ColorPurple,
+	"red":     ColorRed,
+	"scarlet": ColorScarlet,
+	"silver":  ColorSilver,
+	"teal":    ColorTeal,
+	"violet":  ColorViolet,
+	"white":   ColorWhite,
+	"yellow":  ColorYellow,
+}
+
+var (
+	regexOnce         sync.Once
+	colorRegexPats    = make(map[string]*regexp.Regexp)
+	fadeMsecRegexPats = make(map[int]*regexp.Regexp)
+	ledIdxRegexPats   = make(map[int][]*regexp.Regexp)
+
+	errNoColorMatch = errors.New("b1: no color match")
+	errNoFadeMatch  = errors.New("b1: no fade time match")
+	errNoLEDMatch   = errors.New("b1: no LED index match")
+	errBlankQuery   = errors.New("b1: blank query")
+)
+
+func initRegex() {
+	// for colors
+	colorWords := make([]string, 0, len(colorMap))
+	for k := range colorMap {
+		colorWords = append(colorWords, k)
+	}
+	colorRegexPats["name"] = regexp.MustCompile(fmt.Sprintf(`\b(%s)\b`, strings.Join(colorWords, "|")))
+	colorRegexPats["on"] = regexp.MustCompile(`\b(on)\b`)
+	colorRegexPats["off"] = regexp.MustCompile(`\b(off)\b`)
+	colorRegexPats["rgb"] = regexp.MustCompile(`\brgb\s*\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)`)
+	colorRegexPats["hsb"] = regexp.MustCompile(`\bhsb\s*\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)`)
+	colorRegexPats["hex6"] = regexp.MustCompile(`#([0-9a-f]{6})\b`)
+	colorRegexPats["hex3"] = regexp.MustCompile(`#([0-9a-f]{3})\b`)
+
+	// for fade msec
+	fadeMsecRegexPats[0] = regexp.MustCompile(`\b(now|immediate(?:ly)?|instant(?:ly|aneous)?(?:ly)?|quick(?:ly)?|right\b\s*now|swiftly|this\b\s*moment)\b`)
+	fadeMsecRegexPats[1] = regexp.MustCompile(`\b(\d+(?:\.\d+)?)\s*(ms|millis|millisec|millisecs|msec|msecs|millisecond|milliseconds)\b`)
+	fadeMsecRegexPats[1000] = regexp.MustCompile(`\b(\d+(?:\.\d+)?)\s*s(?:ec)?(?:ond)?(?:s)?\b`)
+	fadeMsecRegexPats[60000] = regexp.MustCompile(`\b(\d+(?:\.\d+)?)\s*(m|min|mins|minute|minutes)\b`)
+
+	// for led index
+	ledIdxRegexPats[0] = []*regexp.Regexp{
+		regexp.MustCompile(`\b(?:all\b\s*(leds|led|light|lights)?|(?:all|both)?\b\s*(?:leds|lights)|both)\b`),
+	}
+	ledIdxRegexPats[1] = []*regexp.Regexp{
+		regexp.MustCompile(`\b(?:top|first|1st)\b\s*(led|light)\b`),
+	}
+	ledIdxRegexPats[2] = []*regexp.Regexp{
+		regexp.MustCompile(`\b(?:bottom|second|2nd)\b\s*(led|light)\b`),
+	}
+	ledIdxRegexPats[12] = []*regexp.Regexp{
+		regexp.MustCompile(`\b(led|light)[:#=\s]*([012])\b`),
+		regexp.MustCompile(`\b(led|light)[:#=\s](all|both|zero|one|two)\b`),
+	}
+}
+
+// ParseStateQuery parses the case-insensitive unstructured description of light state and returns the structured LightState.
+// The query can contain information about the color, fade time, and LED index. e.g. turn off all lights right now, set led 1 to color #ff00ff over 2 seconds
+//
+// Color can be specified by name, hex code, or RGB/HSB values, e.g. "red", "#FF0000", "rgb(255,0,0)", "hsb(0,100,100)"
+// Fade time can be specified by milliseconds, seconds, or minutes, e.g. "100ms", "1s", "1.5m", "now", "0s"
+// LED index can be specified by number, name, or position, e.g. "led 1", "led 2", "top led", "second led", "led:all", "led:0"
+//
+// If the query is empty, it returns an error.
+func ParseStateQuery(query string) (LightState, error) {
+	// init regex
+	regexOnce.Do(initRegex)
+
+	// prepare
+	var state LightState
+	query = strings.TrimSpace(strings.ToLower(query))
+	if query == "" {
+		return state, errBlankQuery
+	}
+
+	// parse each part
+	var err error
+	if state.Color, err = parseColor(query); err != nil {
+		return state, err
+	}
+	if state.FadeTime, err = parseFadeTime(query); err != nil {
+		return state, err
+	}
+	if state.LED, err = parseLEDIndex(query); err != nil {
+		return state, err
+	}
+
+	// all done
+	return state, nil
+}
+
+func parseColor(query string) (color.Color, error) {
+	// parse
+	for key, pat := range colorRegexPats {
+		m := pat.FindStringSubmatch(query)
+
+		// not match
+		if m == nil {
+			continue
+		}
+
+		// handle match
+		val := m[1]
+		switch key {
+		case "name":
+			return colorMap[val], nil
+		case "on":
+			return ColorWhite, nil
+		case "off":
+			return ColorBlack, nil
+		case "hex6":
+			var r, g, b uint8
+			n, err := fmt.Sscanf(val, "%02X%02X%02X", &r, &g, &b)
+			if err != nil || n != 3 {
+				return nil, fmt.Errorf("invalid hex6 color: %s - %w", val, err)
+			}
+			return color.RGBA{R: r, G: g, B: b, A: 0xff}, nil
+		case "hex3":
+			var r, g, b uint8
+			n, err := fmt.Sscanf(val, "%1X%1X%1X", &r, &g, &b)
+			if err != nil || n != 3 {
+				return nil, fmt.Errorf("invalid hex3 color: %s - %w", val, err)
+			}
+			return color.RGBA{R: r * 0x11, G: g * 0x11, B: b * 0x11, A: 0xff}, nil
+		case "rgb":
+			var r, g, b uint8
+			n, err := fmt.Sscanf(m[0], "rgb(%d,%d,%d)", &r, &g, &b)
+			if err != nil || n != 3 {
+				return nil, fmt.Errorf("invalid rgb color: %s - %w", val, err)
+			}
+			return color.RGBA{R: r, G: g, B: b, A: 0xff}, nil
+		case "hsb":
+			var h, s, b float64
+			n, err := fmt.Sscanf(m[0], "hsb(%f,%f,%f)", &h, &s, &b)
+			if err != nil || n != 3 {
+				return nil, fmt.Errorf("invalid hsb color: %s - %w", val, err)
+			}
+			return convRGBToColor(convHSBToRGB(h, s, b)), nil
+		}
+	}
+
+	return nil, errNoColorMatch
+}
+
+func parseFadeTime(query string) (time.Duration, error) {
+	// parse
+	for mul, pat := range fadeMsecRegexPats {
+		// skip zero values first
+		if mul == 0 {
+			continue
+		}
+		m := pat.FindStringSubmatch(query)
+
+		// not match
+		if m == nil {
+			continue
+		}
+
+		// handle match
+		if m != nil {
+			if len(m) >= 2 {
+				val, err := strconv.ParseFloat(m[1], 64)
+				if err != nil {
+					return 0, err
+				}
+				return time.Duration(val*float64(mul)) * time.Millisecond, nil
+			}
+		}
+	}
+
+	// handle zero values
+	if m := fadeMsecRegexPats[0].FindStringSubmatch(query); m != nil {
+		return 0, nil
+	}
+	return 0, errNoFadeMatch
+}
+
+func parseLEDIndex(query string) (LEDIndex, error) {
+	// for "led *", "light *", or "led:*" or "led#"
+	for _, pat := range ledIdxRegexPats[12] {
+		m := pat.FindStringSubmatch(query)
+		if m != nil && len(m) >= 3 {
+			switch m[2] {
+			case "0", "all", "both", "zero":
+				return LEDAll, nil
+			case "1", "one":
+				return LED1, nil
+			case "2", "two":
+				return LED2, nil
+			}
+		}
+	}
+
+	// for 1st led
+	for _, pat := range ledIdxRegexPats[1] {
+		m := pat.FindStringSubmatch(query)
+		if m != nil {
+			return LED1, nil
+		}
+	}
+
+	// for 2nd led
+	for _, pat := range ledIdxRegexPats[2] {
+		m := pat.FindStringSubmatch(query)
+		if m != nil {
+			return LED2, nil
+		}
+	}
+
+	// for all/both
+	for _, pat := range ledIdxRegexPats[0] {
+		m := pat.FindStringSubmatch(query)
+		if m != nil {
+			return LEDAll, nil
+		}
+	}
+
+	return 0, errNoLEDMatch
+}
---
+++ parser_test.go
@@ -0,0 +1,354 @@
+package blink1_test
+
+import (
+	"image/color"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/b1ug/blink1-go"
+)
+
+func TestParseStateQuery(t *testing.T) {
+	tests := []struct {
+		query   string
+		want    blink1.LightState
+		wantErr bool
+	}{
+		{
+			query: "turn off all lights right now",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "set led 1 to color #ff00ff over 2 seconds",
+			want:  blink1.LightState{Color: blink1.ColorMagenta, LED: blink1.LED1, FadeTime: 2 * time.Second},
+		},
+		{
+			query: "led=0 color=yellow time:500 ms",
+			want:  blink1.LightState{Color: blink1.ColorYellow, LED: blink1.LEDAll, FadeTime: 500 * time.Millisecond},
+		},
+		{
+			query: "led=1 color=pink fade=10ms",
+			want:  blink1.LightState{Color: blink1.ColorPink, LED: blink1.LED1, FadeTime: 10 * time.Millisecond},
+		},
+		{
+			query: "all leds shift to color #ffff00 immediately",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0xff, B: 0x0, A: 0xff}, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "all leds transition to black in 2 seconds",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LEDAll, FadeTime: 2000 * time.Millisecond},
+		},
+		{
+			query: "change all leds to orange with a fade time of 500 milliseconds",
+			want:  blink1.LightState{Color: blink1.ColorOrange, LED: blink1.LEDAll, FadeTime: 500 * time.Millisecond},
+		},
+		{
+			query: "change led 1 to lime over 5 seconds",
+			want:  blink1.LightState{Color: blink1.ColorLime, LED: blink1.LED1, FadeTime: 5000 * time.Millisecond},
+		},
+		{
+			query: "change light 1 to pink over 5.5 secs",
+			want:  blink1.LightState{Color: blink1.ColorPink, LED: blink1.LED1, FadeTime: 5500 * time.Millisecond},
+		},
+		{
+			query: "change led 2 to color #ff4500 over 4 seconds",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0x45, B: 0x0, A: 0xff}, LED: blink1.LED2, FadeTime: 4000 * time.Millisecond},
+		},
+		{
+			query: "convert led 2 to color #ffc0cb gradually over 3 seconds",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0xc0, B: 0xcb, A: 0xff}, LED: blink1.LED2, FadeTime: 3000 * time.Millisecond},
+		},
+		{
+			query: "fade all leds off this instant",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "fade top led to red now",
+			want:  blink1.LightState{Color: blink1.ColorRed, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query: "fade bottom led to blue in 2secs",
+			want:  blink1.LightState{Color: blink1.ColorBlue, LED: blink1.LED2, FadeTime: 2000 * time.Millisecond},
+		},
+		{
+			query: "fade all leds to purple in 1 second",
+			want:  blink1.LightState{Color: blink1.ColorPurple, LED: blink1.LEDAll, FadeTime: 1000 * time.Millisecond},
+		},
+		{
+			query: "fade led 1 to colour #8b0000 in 1.5 seconds",
+			want:  blink1.LightState{Color: color.RGBA{R: 0x8b, G: 0x0, B: 0x0, A: 0xff}, LED: blink1.LED1, FadeTime: 1500 * time.Millisecond},
+		},
+		{
+			query: "fade led1 to green over 500 milliseconds.",
+			want:  blink1.LightState{Color: blink1.ColorGreen, LED: blink1.LED1, FadeTime: 500 * time.Millisecond},
+		},
+		{
+			query: "fade led 1 to red in 2 seconds.",
+			want:  blink1.LightState{Color: blink1.ColorRed, LED: blink1.LED1, FadeTime: 2000 * time.Millisecond},
+		},
+		{
+			query: "fade led 2 to yellow in 3 seconds",
+			want:  blink1.LightState{Color: blink1.ColorYellow, LED: blink1.LED2, FadeTime: 3000 * time.Millisecond},
+		},
+		{
+			query: "gradually change all leds to color #00ff00 in 2.5 seconds",
+			want:  blink1.LightState{Color: color.RGBA{R: 0x0, G: 0xff, B: 0x0, A: 0xff}, LED: blink1.LEDAll, FadeTime: 2500 * time.Millisecond},
+		},
+		{
+			query: "gradually change led 2 to black in 4 seconds",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LED2, FadeTime: 4000 * time.Millisecond},
+		},
+		{
+			query: "immediately change led 2 to color #8b4513",
+			want:  blink1.LightState{Color: color.RGBA{R: 0x8b, G: 0x45, B: 0x13, A: 0xff}, LED: blink1.LED2, FadeTime: 0},
+		},
+		{
+			query: "immediately set all leds to blue",
+			want:  blink1.LightState{Color: blink1.ColorBlue, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "immediately set led 2 to color white",
+			want:  blink1.LightState{Color: blink1.ColorWhite, LED: blink1.LED2, FadeTime: 0},
+		},
+		{
+			query: "immediately turn led 1 to white",
+			want:  blink1.LightState{Color: blink1.ColorWhite, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query: "immediately turn off bottom led",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LED2, FadeTime: 0},
+		},
+		{
+			query: "instantaneously turn all leds to pink",
+			want:  blink1.LightState{Color: blink1.ColorPink, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "instantly change top led to cyan",
+			want:  blink1.LightState{Color: blink1.ColorCyan, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query: "instantly turn off all leds ",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "instantly turn off led 2",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LED2, FadeTime: 0},
+		},
+		{
+			query: "second led changes to orange immediately",
+			want:  blink1.LightState{Color: blink1.ColorOrange, LED: blink1.LED2, FadeTime: 0},
+		},
+		{
+			query: "1st light fades to color #0f2 over 3 seconds",
+			want:  blink1.LightState{Color: color.RGBA{R: 0x0, G: 0xff, B: 0x22, A: 0xff}, LED: blink1.LED1, FadeTime: 3000 * time.Millisecond},
+		},
+		{
+			query: "led one instantaneously changes to #ffd700 ",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0xd7, B: 0x0, A: 0xff}, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query: "turns off first led right now.",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query: "led2 fades to pink over 5 seconds.",
+			want:  blink1.LightState{Color: blink1.ColorPink, LED: blink1.LED2, FadeTime: 5000 * time.Millisecond},
+		},
+		{
+			query: "led 2 gradually changes to color #ff0000 in 4 seconds.",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0x0, B: 0x0, A: 0xff}, LED: blink1.LED2, FadeTime: 4000 * time.Millisecond},
+		},
+		{
+			query: "2nd led transitions to green quickly.",
+			want:  blink1.LightState{Color: blink1.ColorGreen, LED: blink1.LED2, FadeTime: 0},
+		},
+		{
+			query: "led 2 transitions to yellow slowly over 3 seconds.",
+			want:  blink1.LightState{Color: blink1.ColorYellow, LED: blink1.LED2, FadeTime: 3000 * time.Millisecond},
+		},
+		{
+			query: "led  2 turns blue gradually over 2 seconds.",
+			want:  blink1.LightState{Color: blink1.ColorBlue, LED: blink1.LED2, FadeTime: 2000 * time.Millisecond},
+		},
+		{
+			query: "led:2, color:green, time:1s",
+			want:  blink1.LightState{Color: blink1.ColorGreen, LED: blink1.LED2, FadeTime: 1000 * time.Millisecond},
+		},
+		{
+			query: "led:all color:blue fade:500ms",
+			want:  blink1.LightState{Color: blink1.ColorBlue, LED: blink1.LEDAll, FadeTime: 500 * time.Millisecond},
+		},
+		{
+			query: "make led#2 purple now",
+			want:  blink1.LightState{Color: blink1.ColorPurple, LED: blink1.LED2, FadeTime: 0},
+		},
+		{
+			query: "quickly turn all leds to colour #fa8072 in 1 second",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xfa, G: 0x80, B: 0x72, A: 0xff}, LED: blink1.LEDAll, FadeTime: 1000 * time.Millisecond},
+		},
+		{
+			query: "red for all in 1s",
+			want:  blink1.LightState{Color: blink1.ColorRed, LED: blink1.LEDAll, FadeTime: 1000 * time.Millisecond},
+		},
+		{
+			query: "set all led to #00ffff this moment",
+			want:  blink1.LightState{Color: color.RGBA{R: 0x0, G: 0xff, B: 0xff, A: 0xff}, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "set all light to blue instantly.",
+			want:  blink1.LightState{Color: blink1.ColorBlue, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "set all lights to color #ffeeab immediately.",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0xee, B: 0xab, A: 0xff}, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "set all leds to magenta over 2 seconds",
+			want:  blink1.LightState{Color: blink1.ColorMagenta, LED: blink1.LEDAll, FadeTime: 2000 * time.Millisecond},
+		},
+		{
+			query: "set all leds to red instantly",
+			want:  blink1.LightState{Color: blink1.ColorRed, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "set led:1 to blue in 500ms",
+			want:  blink1.LightState{Color: blink1.ColorBlue, LED: blink1.LED1, FadeTime: 500 * time.Millisecond},
+		},
+		{
+			query: "set led #1 to green instantly",
+			want:  blink1.LightState{Color: blink1.ColorGreen, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query: "set led  1 to purple right now",
+			want:  blink1.LightState{Color: blink1.ColorPurple, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query: "set led  2 to blue right now",
+			want:  blink1.LightState{Color: blink1.ColorBlue, LED: blink1.LED2, FadeTime: 0},
+		},
+		{
+			query: "set led:2 green over 1 second",
+			want:  blink1.LightState{Color: blink1.ColorGreen, LED: blink1.LED2, FadeTime: 1000 * time.Millisecond},
+		},
+		{
+			query: "slowly change all leds to color #add8e6 in 4.5 seconds",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xad, G: 0xd8, B: 0xe6, A: 0xff}, LED: blink1.LEDAll, FadeTime: 4500 * time.Millisecond},
+		},
+		{
+			query: "slowly fade all leds to black in 5 seconds",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LEDAll, FadeTime: 5000 * time.Millisecond},
+		},
+		{
+			query: "slowly fade all leds to color #00f over 1.5 seconds.",
+			want:  blink1.LightState{Color: color.RGBA{R: 0x0, G: 0x0, B: 0xff, A: 0xff}, LED: blink1.LEDAll, FadeTime: 1500 * time.Millisecond},
+		},
+		{
+			query: "transition all leds to white over a period of 500 milliseconds",
+			want:  blink1.LightState{Color: blink1.ColorWhite, LED: blink1.LEDAll, FadeTime: 500 * time.Millisecond},
+		},
+		{
+			query: "transition led 1 to color #808080 in 2.5 seconds",
+			want:  blink1.LightState{Color: color.RGBA{R: 0x80, G: 0x80, B: 0x80, A: 0xff}, LED: blink1.LED1, FadeTime: 2500 * time.Millisecond},
+		},
+		{
+			query: "turn all leds to #f08080 swiftly over 1 second",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xf0, G: 0x80, B: 0x80, A: 0xff}, LED: blink1.LEDAll, FadeTime: 1000 * time.Millisecond},
+		},
+		{
+			query: "turn led 1 to color #ffa500 over 3 seconds",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0xa5, B: 0x0, A: 0xff}, LED: blink1.LED1, FadeTime: 3000 * time.Millisecond},
+		},
+		{
+			query: "turn led 2 to #ffa500 over 3 seconds",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0xa5, B: 0x0, A: 0xff}, LED: blink1.LED2, FadeTime: 3000 * time.Millisecond},
+		},
+		{
+			query: "turn off all leds this instant",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "turn on all now",
+			want:  blink1.LightState{Color: blink1.ColorWhite, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "rgb(255, 0, 255) for both now",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0x0, B: 0xff, A: 0xff}, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "rgb(255,255,0) for all now",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0xff, B: 0x0, A: 0xff}, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "hsb(195, 100, 100) for led 1 instantly",
+			want:  blink1.LightState{Color: color.RGBA{R: 0x0, G: 0xbf, B: 0xff, A: 0xff}, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query: "set hsb(315,100,100) for led2 right now",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0x0, B: 0xbf, A: 0xff}, LED: blink1.LED2, FadeTime: 0},
+		},
+		{
+			query: "all lights off now",
+			want:  blink1.LightState{Color: blink1.ColorBlack, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: "set red for led#2 in 2 secs",
+			want:  blink1.LightState{Color: blink1.ColorRed, LED: blink1.LED2, FadeTime: 2000 * time.Millisecond},
+		},
+		{
+			query: "make led one blue over 600 msecs",
+			want:  blink1.LightState{Color: blink1.ColorBlue, LED: blink1.LED1, FadeTime: 600 * time.Millisecond},
+		},
+		{
+			query: "turn both light green within 800ms",
+			want:  blink1.LightState{Color: blink1.ColorGreen, LED: blink1.LEDAll, FadeTime: 800 * time.Millisecond},
+		},
+		{
+			query: "🎨(color=#800080 led=1 fade=0ms)",
+			want:  blink1.LightState{Color: color.RGBA{R: 0x80, G: 0x0, B: 0x80, A: 0xff}, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query: "🎨(color=#FFA500 led=2 fade=3s)",
+			want:  blink1.LightState{Color: color.RGBA{R: 0xff, G: 0xa5, B: 0x0, A: 0xff}, LED: blink1.LED2, FadeTime: 3000 * time.Millisecond},
+		},
+		{
+			query: `all pink now`,
+			want:  blink1.LightState{Color: blink1.ColorPink, LED: blink1.LEDAll, FadeTime: 0},
+		},
+		{
+			query: `led=1 color=yellow time=500ms`,
+			want:  blink1.LightState{Color: blink1.ColorYellow, LED: blink1.LED1, FadeTime: 500 * time.Millisecond},
+		},
+		{
+			query: `led=1 color=yellow now`,
+			want:  blink1.LightState{Color: blink1.ColorYellow, LED: blink1.LED1, FadeTime: 0},
+		},
+		{
+			query:   `led=1 color=yellow`,
+			wantErr: true,
+		},
+		{
+			query:   `color=yellow now`,
+			wantErr: true,
+		},
+		{
+			query:   `led color=yellow time=500ms`,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.query, func(t *testing.T) {
+			got, err := blink1.ParseStateQuery(tt.query)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseStateQuery(%q) got error = %v, wantErr %v", tt.query, err, tt.wantErr)
+				return
+			}
+			if err != nil {
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseStateQuery(%q) = %v, want %v", tt.query, got, tt.want)
+			}
+		})
+	}
+}
